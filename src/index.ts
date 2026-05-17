ab718b96f3de48480bfc17fa60f03b63acbd3bdf
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-05-17 16:23:10 +0530

    add index

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+// Environment variables
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL || "";
+
+// Global middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/v1/auth");
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
