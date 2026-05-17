386c0fcb7a96b15cf0e21f41187934ecda7dcb38
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-05-17 16:53:02 +0530

    add authController

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,5 +1,62 @@
 import { Request, Response } from "express";
+import UserModel, { UserRole } from "../models/userModel";
+import bcrypt from "bcryptjs";
 
-export const registerUser = async (req: Request, res: Response) => {};
+// User registration
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
 
-export const loginUser = async (req: Request, res: Response) => {};
+    // Check if user already exists
+    const existingUser = await UserModel.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email already exists" });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hashSync(password, salt);
+
+    // Create new user
+    const newUser = new UserModel({
+      username,
+      email,
+      password: hashedPassword,
+      roles: [UserRole.USER], // Default role
+    });
+
+    // Save user to database
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error registering user" });
+  }
+};
+
+// User login
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find user by username
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    // Check if the password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid username or password" });
+    }
+
+    res.status(200).json({ message: "User logged in successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in user" });
+  }
+};
