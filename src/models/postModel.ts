3e2e1e47b3481ee503c5c4f35b47ec830b9aaf05
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-05-20 11:52:09 +0530

    add post model

---
+++ src/models/postModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IPost extends Document {
+  title: string;
+  description: string;
+  author: mongoose.Types.ObjectId;
+  imageURL?: string[];
+  tags?: string[];
+  comments?: mongoose.Types.ObjectId[];
+  bookmark?: mongoose.Types.ObjectId[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const postSchema = new mongoose.Schema<IPost>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    imageURL: [{ type: String }],
+    tags: [{ type: String }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    bookmark: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true },
+);
+
+export const PostModel = mongoose.model<IPost>("Post", postSchema);
